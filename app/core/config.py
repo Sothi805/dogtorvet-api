898399640e7556971323fc32e0f85c270fa37219
Thi Thi <vethsothi002@gmail.com>
2025-07-29 00:14:17 +0700

    lost I

--- app/core/config.py
+++ app/core/config.py
@@ -37,7 +37,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
         "https://dogtorvetservices.onrender.com",
-        "https://dogtorvet-ui.onrender.com"
+        "https://dogtorvetservices.vercel.app"
     ]
     
     # Admin User Settings
